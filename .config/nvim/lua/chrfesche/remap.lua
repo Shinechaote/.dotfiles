7d2e2193043db88901c7f11ff7cb6c232cb0b8f6
Christian Scherer <felix.scherer@stud.tu-darmstadt.de>
2024-10-13 19:13:14 +0200

    remap explorer shortcut

--- .config/nvim/lua/chrfesche/remap.lua
+++ .config/nvim/lua/chrfesche/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", vim.cmd.Ex)
 
 local pattern = [[\v['"`({[< ]@<=([a-zA-Z0-9])|^([a-zA-Z0-9])|(.)$|([][(){}<>.,;_+-])@<=([a-zA-Z0-9])|(['"])@<=([][(){}<>.,;_+-\$])(['"])]]
 vim.keymap.set({'n', 'v'}, 'w', function()
